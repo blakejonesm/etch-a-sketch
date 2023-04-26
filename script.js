80618451437cc8f0df1a7579eebb93f2fdd9dd9c
blake <97263263+blakejonesm@users.noreply.github.com>
2023-04-26 16:11:48 +1000

    Define js variables based off html id and classes
    Also added a class to color picker and slider

---
+++ script.js
@@ -0,0 +1,10 @@
+let container = document.querySelector(".container");
+let containerSize = 16;
+
+let colorSelector = document.querySelector("#colorSelector");
+let colorBtn = document.querySelector("#colorBtn");
+let rainbowBtn = document.querySelector("#rainbowBtn");
+let eraserBtn = document.querySelector("#eraserBtn");
+let clearBtn = document.querySelector("#clearBtn");
+let sliderValue = document.querySelector(".sliderValue");
+let slider = document.querySelector(".slider");
