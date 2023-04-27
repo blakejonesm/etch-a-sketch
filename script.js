51b2e94a8a15989e517116f63a1c8cd286472fe6
blake <97263263+blakejonesm@users.noreply.github.com>
2023-04-27 21:29:12 +1000

    Add functionality to random erase and clear button
    Changed from switch function to else if's as it is more simple.

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let container = document.querySelector("#container");
-let containerSize = 16;
+const DEFAULT_COLOR = '#333333'
+const DEFAULT_MODE = 'color'
+const DEFAULT_SIZE = 16
 
+let containerSize = DEFAULT_SIZE;
+let currentMode = DEFAULT_MODE;
+let currentColor = DEFAULT_COLOR;
+
+function setCurrentColor(newColor) {
+    currentColor = newColor
+}
+
+function setCurrentMode(newMode) {
+    currentMode = newMode
+}
+
+let container = document.querySelector("#container");
 let colorSelector = document.querySelector("#colorSelector");
 let colorBtn = document.querySelector("#colorBtn");
 let rainbowBtn = document.querySelector("#rainbowBtn");
@@ -8,6 +22,12 @@ let eraserBtn = document.querySelector("#eraserBtn");
 let clearBtn = document.querySelector("#clearBtn");
 let sliderValue = document.querySelector("#sliderValue");
 let slider = document.querySelector("#slider");
+
+colorSelector.oninput = (e) => setCurrentColor(e.target.value)
+colorBtn.onclick = () => setCurrentMode('color')
+rainbowBtn.onclick = () => setCurrentMode('rainbow')
+eraserBtn.onclick = () => setCurrentMode('eraser')
+clearBtn.onclick = () => reloadContainer()
 
 const percentage = 100;
 
@@ -17,28 +37,43 @@ function createGrid(size) {
         cell.classList.add("cell");
         cell.style.height = `calc(${percentage}% / ${size})`;
         cell.style.width = `calc(${percentage}% / ${size})`;
-        cell.style.backgroundColor = "white";
+        cell.style.backgroundColor = "#FFFFFF";
+        cell.addEventListener('mouseover', changeColor)
+        cell.addEventListener('mousedown', changeColor)
 
         container.append(cell);
     };
 };
 
-function updateContainerSize(size) {
-    container.innerHTML = '';
+function changeColor(e) {
+    if (currentMode === 'rainbow') {
+        const randomRed = Math.floor(Math.random() * 256)
+        const randomGreen = Math.floor(Math.random() * 256)
+        const randomBlue = Math.floor(Math.random() * 256)
+        e.target.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`
+    } else if (currentMode === 'color') {
+        e.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'eraser') {
+        e.target.style.backgroundColor = '#ffffff'
+    }
+}
+
+function changeContainerSize(size) {
+    clearContainer();
     createGrid(size);
 };
 
+function reloadContainer() {
+    clearContainer();
+    createGrid(containerSize);
+}
+
+function clearContainer() {
+    container.innerHTML = ''
+}
+
 function updateSliderValue() {
     sliderValue.innerText = `${slider.value} x ${slider.value}`;
-};
-
-function preSelectedButton() {
-    colorBtn.focus();
-    
-    addGlobalEventListener("mousedown", ".cell", e => {
-        colorBtn.focus();
-        e.target.style.background = colorSelector.value;
-    });
 };
 
 function addGlobalEventListener(type, selector, callback) {
@@ -49,10 +84,10 @@ function addGlobalEventListener(type, selector, callback) {
 
 addGlobalEventListener("change", "#slider", e => {
     updateSliderValue(e.target.value)
-    updateContainerSize(e.target.value)
+    changeContainerSize(e.target.value)
   });
+
 
 window.onload = () => {
     createGrid(containerSize);
-    preSelectedButton()
 };
