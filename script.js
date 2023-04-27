abdbbda56b6910628ec42902087f6a2a4d051a62
blake <97263263+blakejonesm@users.noreply.github.com>
2023-04-27 16:57:28 +1000

    Make container responsive to slider input value

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let container = document.querySelector(".container");
+let container = document.querySelector("#container");
 let containerSize = 16;
 
 let colorSelector = document.querySelector("#colorSelector");
@@ -6,42 +6,49 @@ let colorBtn = document.querySelector("#colorBtn");
 let rainbowBtn = document.querySelector("#rainbowBtn");
 let eraserBtn = document.querySelector("#eraserBtn");
 let clearBtn = document.querySelector("#clearBtn");
-let sliderValue = document.querySelector(".sliderValue");
-let slider = document.querySelector(".slider");
+let sliderValue = document.querySelector("#sliderValue");
+let slider = document.querySelector("#slider");
 
 const percentage = 100;
 
 function createGrid(size) {
     for (let i = 1; i <= size * size; i++) {
         let cell = document.createElement("div");
+        cell.classList.add("cell");
         cell.style.height = `calc(${percentage}% / ${size})`;
         cell.style.width = `calc(${percentage}% / ${size})`;
         cell.style.backgroundColor = "white";
 
         container.append(cell);
     };
-}
+};
 
 function updateContainerSize(size) {
     container.innerHTML = '';
     createGrid(size);
-}
+};
 
 function updateSliderValue() {
     sliderValue.innerText = `${slider.value} x ${slider.value}`;
-}
+};
 
 function preSelectedButton() {
     colorBtn.focus();
-}
+
+};
 
 function addGlobalEventListener(type, selector, callback) {
     document.addEventListener(type, e => {
-        if(e.taret.matches(selector)) callback(e)
+        if(e.target.matches(selector)) callback(e)
     })
 }
+
+addGlobalEventListener("change", "#slider", e => {
+    updateSliderValue(e.target.value)
+    updateContainerSize(e.target.value)
+  });
 
 window.onload = () => {
     createGrid(containerSize);
     preSelectedButton()
-}
+};
